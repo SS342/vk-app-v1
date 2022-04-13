10efeb659ed9657516964c7d1561633d996992dc
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-04-13 11:49:19 +0300

    Add files via upload

---
+++ src/panels/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+
+const Home = ({ id, go, fetchedUser }) => (
+	<Panel id={id}>
+		<PanelHeader>Example</PanelHeader>
+		
+
+		<Group header={<Header mode="secondary">Navigation Example</Header>}>
+			<Div>
+				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
+						АРТЁМ ПИДОР
+				</Button>
+			</Div>
+		</Group>
+	</Panel>
+);
+
+Home.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Home;
